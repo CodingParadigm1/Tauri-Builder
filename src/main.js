fe41a7d31cbc5023ad58d0225daa4a4192ccb974
CodingParadigm1 <124928232+CodingParadigm1@users.noreply.github.com>
2023-02-10 13:19:05 -0700

    altered app render order
    Visuals load before Tauri commands are executed. Further reduced amount of code.

--- src/main.js
+++ src/main.js
@@ -1,16 +1,18 @@
 import "./style.css";
 import App from "./App.svelte";
 import "@fontsource/rubik"; 
-import { writable } from "svelte/store";
 import { invoke } from "@tauri-apps/api";
+
 const app = new App({
   target: document.getElementById("app"),
 });
-let projects = [];
+export default app;
 
+let projects = [];
 (async ()=>{
   projects = await invoke('get_dir', {curPath: "./"})
 })();
+
 (async () => {
   let counter = 0; 
   while (counter<projects.length){
@@ -21,4 +23,3 @@ let projects = [];
   }
   await invoke('generate_tauri_app', {framework:"initial", appname: ""});
 })(); 
-export default app;
