e908fb5f1623979f1176750999760c6911f71cdb
CodingParadigm1 <124928232+CodingParadigm1@users.noreply.github.com>
2023-02-10 14:07:49 -0700

    improved functionality & visuals
    removed excess classes, then moved click event to section. Span element has also been replaced with a section. This allowed icons to appear more centered.

--- src/lib/MainPage/FrameworkLogo.svelte
+++ src/lib/MainPage/FrameworkLogo.svelte
@@ -2,32 +2,33 @@
     export let icon_img = ""; 
 </script>
 
-<span>
-    <img src={icon_img} alt="#" class="icon" on:click>
-</span>
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<section on:click>
+    <img src={icon_img} alt="#">
+</section>
 
 <style>
-    .icon{
+    img{
         width:65px;
         height:65px;
     }
-    .icon:hover{
+    img:hover{
         width:70px;
         height:70px;
     }
-    img{
-        margin-top:2px;
-    }
-    span{
+    section{
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
         width:75px;
         height:75px;
         padding:2px;
-        text-align: center;
         transition:.4s; 
         border-radius: 50%;
+        background-image: radial-gradient( rgb(99, 98, 98), black);
     }
-    span:hover{
-        background-image: radial-gradient( rgb(80, 77, 77), black);
+    section:hover{
         padding:5px;
     }
 </style>
