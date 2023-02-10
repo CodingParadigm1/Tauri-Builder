7ed7ce2aec1801715c016b8a2e2a30ce6c7e4434
CodingParadigm1 <124928232+CodingParadigm1@users.noreply.github.com>
2023-02-10 14:26:56 -0700

    updated pages
    updated pages, and added license component.

--- src/stores/page_comps.js
+++ src/stores/page_comps.js
@@ -2,10 +2,11 @@ import { writable } from "svelte/store";
 import FrameWork from "../lib/FrameWork/+page.svelte"; 
 import MainPage from '../lib/MainPage/+page.svelte'; 
 import Projects from '../lib/Projects/+page.svelte'; 
-
+import License from '../lib/License/+page.svelte'; 
 
 export let page_component = writable([
-    {"page":MainPage,"title":"main menu", "index":0}, 
-    {"page":FrameWork,"title":"frame works", "index":1}, 
-    {"page":Projects, "title":"projects","index":2}
+    {"page":MainPage,"title":"Home", "index":0}, 
+    {"page":FrameWork,"title":"frameworks", "index":1}, 
+    {"page":Projects, "title":"projects","index":2}, 
+    {"page": License, "title":"license", "index": 3}
 ]); 
