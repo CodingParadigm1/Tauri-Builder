faaebcee091fd241e8212759cb0ee94ad5c0e956
CodingParadigm1 <124928232+CodingParadigm1@users.noreply.github.com>
2023-02-11 22:44:14 -0700

    parallel functions
    app renders independently from function calls.

--- src/main.js
+++ src/main.js
@@ -2,14 +2,26 @@ import "./style.css";
 import App from "./App.svelte";
 import "@fontsource/rubik"; 
 import { invoke } from "@tauri-apps/api";
-
-const app = new App({
-  target: document.getElementById("app"),
-});
-export default app;
-
 let projects = [];
-(async ()=>{
-  projects = await invoke('get_dir', {curPath: "./"}); 
-  await invoke('scan_files', {titles: projects}); 
-})(); 
+
+let render = async ()=>{
+  const app = async () =>{ 
+    return new App({
+      target: document.getElementById("app"),
+    })
+  };
+  const develop_data = async ()=>{
+    projects = await invoke('get_dir', {curPath: "./"}); 
+    await invoke('scan_files', {titles: projects}); 
+  };
+  return {
+    projs: develop_data(), 
+    app_data:  app()
+  }
+}; 
+
+export default (await render()).app_data
+
+
+
+ 
