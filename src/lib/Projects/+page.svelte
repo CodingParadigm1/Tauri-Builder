bc1959650b07e1b060785ca847a8616854280b12
CodingParadigm1 <124928232+CodingParadigm1@users.noreply.github.com>
2023-02-11 18:16:31 -0700

    updated project page
    Array for projects is now consisting of objects. 
    provided further explanation of how to use cards, after added functionality.

--- src/lib/Projects/+page.svelte
+++ src/lib/Projects/+page.svelte
@@ -1,20 +1,30 @@
 <script>
-    import TitlePage from "../MainPage/TitlePage.svelte";
+	import TitlePage from './../MainPage/TitlePage.svelte';
     import ProjectsList from "./ProjectsList.svelte";
     import {invoke} from "@tauri-apps/api"; 
     import { entire_dir } from "../../stores/gen_dir";
     let description = "Below are your created projects. If there are no projects, this app has created an example of one for you."
-
+    let hint = "Tip: click the card to open vscode. Click 'delete' to delete the project."; 
     let projects = []; 
+    
     (async ()=>{
             projects = await invoke('get_dir', {curPath: "./"})
     })();
-    $: entire_dir.set(projects); 
+    let indexed_projs = []; 
+    $: (()=>{
+        let counter = 0; 
+        while (counter<projects.length){
+            let indexed_project = {"title": projects[counter], "index": counter, "showBtn": false}; 
+            indexed_projs = [indexed_project, ...indexed_projs]; 
+            ++counter; 
+    }})(); 
+    $: entire_dir.set(indexed_projs); 
 </script>
 
 <main>
     <TitlePage title="Projects" desc={description}/>
     <ProjectsList/>
+    <TitlePage title="" desc={hint}/>
 </main>
 
 <style>
