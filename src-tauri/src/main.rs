81dff0960fb285de8b92487dd32153e000427327
CodingParadigm1 <124928232+CodingParadigm1@users.noreply.github.com>
2023-02-11 18:19:28 -0700

    added ability to erase projects

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,12 +3,11 @@
     windows_subsystem = "windows"
 )]
 mod projects;
-use projects::manage::{get_dir, generate_tauri_app, open_vscode};  
-
+use projects::manage::{get_dir, generate_tauri_app, open_vscode, erase_project};  
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![get_dir, generate_tauri_app, open_vscode])
+        .invoke_handler(tauri::generate_handler![get_dir, generate_tauri_app, open_vscode, erase_project])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
